ebf0b67782523c33dddf1ff83181d4d12737923c
AndreSailing <Andrea.unipd@studenti.unipd.it>
2024-12-13 16:07:40 +0100

    E6B2

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <Arduino.h>
 #include <SailtrackModule.h>
+#include <Encoder.h>
 
 // -------------------------- Configuration -------------------------- //
+//pin E6B2-CWZ3E
+#define ENCODER_PIN_A 21
+#define ENCODER_PIN_B 17 
+#define ENCODER_PIN_Z 16
 
 #define BATTERY_ADC_PIN 			35
 #define BATTERY_ADC_RESOLUTION 		4095
@@ -26,10 +31,31 @@ class ModuleCallbacks: public SailtrackModuleCallbacks {
 	}
 };
 
+Encoder myEncoder(ENCODER_PIN_A, ENCODER_PIN_B);
+
+long oldPosition  = -999;
+long newPosition;
+
 void setup() {
-	stm.begin("sailtrack-flight-control", IPAddress(192, 168, 42, 115), new ModuleCallbacks());
+  Serial.begin(115200); // Inizializzazione della comunicazione seriale
+  pinMode(ENCODER_PIN_Z, INPUT_PULLUP); // Configurazione del pin Z come input con pull-up
 }
 
 void loop() {
+  newPosition = myEncoder.read(); // Lettura della posizione dell'encoder
 
+  if (newPosition != oldPosition) { // Se la posizione è cambiata
+    Serial.print("Posizione: ");
+    Serial.println(newPosition); // Stampa la nuova posizione
+    oldPosition = newPosition;
+  }
+
+  // Gestione del segnale Z (indice)
+  if (digitalRead(ENCODER_PIN_Z) == LOW) {
+    Serial.println("Segnale Z rilevato!");
+    myEncoder.write(0); // Resetta la posizione dell'encoder
+  }
+
+  delay(100); // Ritardo per evitare letture troppo frequenti
 }
+
